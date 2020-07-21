38394c81cb65ae4c875ca2051a35ea32dadd296d
Daivuk <daivuk@gmail.com>
2020-07-21 08:13:25 -0400

    Updated relay to protocol 4.5.6

--- RelayTestApp/src/GameScreen.js
+++ RelayTestApp/src/GameScreen.js
@@ -6,6 +6,9 @@ let colors = require('./Colors').colors
 // user
 // lobby
 // pings
+// relayOptions
+//   reliable
+//   ordered
 class GameScreen extends Component
 {
     constructor()
@@ -31,27 +34,61 @@ class GameScreen extends Component
     {
         this.props.onPlayerShockwave(this.mousePos)
     }
-    
+
+    onToggleReliable(e)
+    {
+        this.props.onToggleReliable()
+    }
+
+    onToggleOrdered(e)
+    {
+        this.props.onToggleOrdered()
+    }
+
+    onTogglePlayerMask(profileId)
+    {
+        this.props.onTogglePlayerMask(profileId)
+    }
+
     render()
     {
         return (
             <div className="GameScreen">
-                <div className="GamePlayArea" ref="GamePlayArea" style={{cursor: `url('arrow${this.props.user.colorIndex}.png'), auto`}} onMouseMove={this.onMouseMove.bind(this)} onMouseDown={this.onShocwave.bind(this)}>
-                    {
-                        this.props.shockwaves.map(shockwave => (
-                            <div key={`${shockwave.id}`} className="Entity" style={{left: `${shockwave.pos.x-64}px`, top: `${shockwave.pos.y-64}px`}}>
-                                <div className="Shockwave" style={{backgroundColor: shockwave.color}}></div>
-                            </div>
-                        ))
-                    }
-                    {
-                        this.props.lobby.members.filter(member => member.pos && member.profileId != this.props.user.id).map(member => (
-                            <div key={`${member.profileId}_arrow`} className="Entity" style={{left: `${member.pos.x}px`, top: `${member.pos.y}px`}}>
-                                <img className="Arrow" src={`arrow${member.extra.colorIndex}.png`} alt="arrow"/>
-                                <p style={{color: colors[member.extra.colorIndex]}}>{member.name}</p>
-                            </div>
-                        ))
-                    }
+                <div>
+                    <div className="OptionPanel" ref="OptionPanel" style={{float:"left", paddingRight:32, textAlign:"left"}}>
+                        <p>Player Mask (For shockwaves)</p>
+                        {
+                            this.props.lobby.members.map(member => (
+                                <div key={`${member.profileId}_mask`}>
+                                    <input type="checkbox" name={`${member.profileId}_mask`} onChange={() => this.onTogglePlayerMask(member.profileId)} defaultChecked={member.allowSendTo}/>
+                                    <label htmlFor={`${member.profileId}_mask`}>{member.name}</label>
+                                </div>
+                            ))
+                        }
+                        <p>Reliable options (For mouse position)</p>
+                        <input type="checkbox" key="chkReliable" name="chkReliable" onChange={this.onToggleReliable.bind(this)} defaultChecked={this.props.relayOptions.reliable}/>
+                        <label htmlFor="chkReliable">Reliable</label><br/>
+                        <input type="checkbox" key="chkOrdered" name="chkOrdered" onChange={this.onToggleOrdered.bind(this)} defaultChecked={this.props.relayOptions.ordered}/>
+                        <label htmlFor="chkOrdered">Ordered</label>
+                    </div>
+                    <div className="GamePlayArea" ref="GamePlayArea" style={{cursor: `url('arrow${this.props.user.colorIndex}.png'), auto`, float:"left"}} 
+                        onMouseMove={this.onMouseMove.bind(this)} onMouseDown={this.onShocwave.bind(this)}>
+                        {
+                            this.props.shockwaves.map(shockwave => (
+                                <div key={`${shockwave.id}`} className="Entity" style={{left: `${shockwave.pos.x-64}px`, top: `${shockwave.pos.y-64}px`}}>
+                                    <div className="Shockwave" style={{backgroundColor: shockwave.color}}></div>
+                                </div>
+                            ))
+                        }
+                        {
+                            this.props.lobby.members.filter(member => member.pos && member.profileId !== this.props.user.id).map(member => (
+                                <div key={`${member.profileId}_arrow`} className="Entity" style={{left: `${member.pos.x}px`, top: `${member.pos.y}px`}}>
+                                    <img className="Arrow" src={`arrow${member.extra.colorIndex}.png`} alt="arrow"/>
+                                    <p style={{color: colors[member.extra.colorIndex]}}>{member.name}</p>
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
                 <button className="Button" onClick={this.onBack.bind(this)}>Leave Game</button>
             </div>
